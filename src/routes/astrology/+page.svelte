b5974adc8dd75813739b9864b72e20d045522696
Diego Avendaño <digago7@gmail.com>
2023-07-13 21:27:39 -0600

    slider

--- src/routes/astrology/+page.svelte
+++ src/routes/astrology/+page.svelte
@@ -1,19 +1,37 @@
 <script>
-	export let images = ['../../lib/birthchart.jpg', '../../lib/partner.jpg'];
+	import birthChart from '$lib/birth-chart.avif';
+	import novastraLogo2 from '$lib/novastra-logo2.webp';
+	import space from '$lib/birthchart.jpg';
+	import lizi from '$lib/lizi.webp';
+	import circleFull from '$lib/circle-full.svg';
+	import circleEmpty from '$lib/circle-empty.svg';
+
+	let images = [birthChart, novastraLogo2, space, lizi];
 	let currentImage = 0;
 
-	function nextImage() {
-		currentImage = (currentImage + 1) % images.length;
+	function changeImage(index) {
+		currentImage = index;
 	}
 
-	function previousImage() {
-		currentImage = (currentImage - 1 + images.length) % images.length;
+	function handleKeyPress(event, index) {
+		if (event.key === 'Enter' || event.key === ' ') {
+			changeImage(index);
+		}
 	}
 </script>
 
-<div class="carousel">
-	<img src={images[currentImage]} alt="carousel" />
+<div class="flex justify-center h-[28rem] px-3">
+	<img class="w-full h-full object-cover" src={images[currentImage]} alt="carousel" />
 </div>
 
-<button on:click={previousImage}>Previous</button>
-<button on:click={nextImage}>Next</button>
+<div class="flex justify-center my-2 gap-2">
+	{#each images as image, index}
+		<button
+			tabindex="0"
+			on:click={() => changeImage(index)}
+			on:keydown={(event) => handleKeyPress(event, index)}
+		>
+			<img class="w-4" src={currentImage === index ? circleEmpty : circleFull} alt="circle" />
+		</button>
+	{/each}
+</div>
