c5afe1f3dd4d8e151218a29f874d89e23900c71b
Diego Avendaño <digago7@gmail.com>
2023-07-29 18:33:57 -0600

    blog

---
+++ src/posts/first-post.md
@@ -0,0 +1,19 @@
+---
+title: First post
+description: First post.
+date: '2023-4-11'
+categories:
+  - sveltekit
+  - svelte
+published: true
+---
+
+## Markdown
+
+Hey firends! 👋
+
+```js
+function greet(name) {
+	console.log(`Hey ${name}! 👋`)
+}
+```
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,22 @@
+<script>
+	import * as config from '$lib/config'
+	import { formatDate } from '$lib/utils.js'
+
+	export let data
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
 blog
+
+<section>
+	{#each data.posts as post}
+		<li>
+			<a href="/blog/{post.slug}">{post.title}</a>
+			<p>{formatDate(post.date)}</p>
+			<p>{post.description}</p>
+		</li>
+	{/each}
+</section>
